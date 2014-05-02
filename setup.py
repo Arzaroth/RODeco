4d63278960229a50e413257c4f03b4c27d6a390a
Arzaroth <lekva@arzaroth.com>
2014-05-02 11:05:15 +0200

    adding download url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     version='1.0',
     license='GPLv3',
     url='https://github.com/Arzaroth/RODeco',
+    download_url='https://github.com/Arzaroth/RODeco/tarball/1.0',
     author='Lucas Philippe, Marc-Etienne Barrut',
     author_email='lu.k.philippe@gmail.com, me.barrut@gmail.com',
     description='RODeco a read only function decorator',
