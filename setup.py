1def5ddb787b7fa27f80dd1bd84f31ae00e19e36
Arzaroth <lekva@arzaroth.com>
2014-05-02 10:51:10 +0200

    specify url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     name='RODeco',
     version='1.0',
     license='GPLv3',
+    url='https://github.com/Arzaroth/RODeco',
     author='Lucas Philippe, Marc-Etienne Barrut',
     author_email='lu.k.philippe@gmail.com, me.barrut@gmail.com',
     description='RODeco a read only function decorator',
