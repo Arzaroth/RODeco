7478793480c2b35fc3159c99785400026732f440
Arzaroth Lekva <lekva@arzaroth.com>
2015-01-04 22:31:59 +0900

    version number

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import setuptools
 
 setuptools.setup(
     name='RODeco',
-    version='1.0',
+    version='1.1',
     license='GPLv3',
     url='https://github.com/Arzaroth/RODeco',
-    download_url='https://github.com/Arzaroth/RODeco/tarball/1.0',
+    download_url='https://github.com/Arzaroth/RODeco/tarball/1.1',
     author='Lucas Philippe, Marc-Etienne Barrut',
     author_email='lu.k.philippe@gmail.com, me.barrut@gmail.com',
     description='RODeco a read only function decorator',
